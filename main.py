61bd6d9f4c5e6ddff13237d889cec85a95862e55
chiefkufre <samuelkufrewillie@gmail.com>
2022-11-17 02:19:15 +0100

    chore: initialize app and register routes

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # application initilization starts here
+
+from typing import List
+from fastapi import FastAPI, status, Response, HTTPException, Depends
+
+
+
+# internal import
+
+from routers import auth, user, weather
+
+
+# Application initilization
+app = FastAPI()
+
+
+# Registering routes
+app.include_router(auth.router)
+app.include_router(user.router)
+app.include_router(weather.router)
+
