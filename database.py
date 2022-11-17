27ca7841df7ec9c638dceb9f21ad5c7478fad130
chiefkufre <samuelkufrewillie@gmail.com>
2022-11-17 02:34:23 +0100

    chore: create a temporary db engine with sqlachemy

--- database.py
+++ database.py
@@ -1,3 +1,30 @@
 
 
 # application engine configuration goes here
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+USER = os.getenv('USERNAME')
+PASSWORD = os.environ.get('PASSWORD')
+# created a temporary database engine with postgres to test my endpoint
+SQLALCHEMY_DATABASE_URL = "postgresql://{USER}:{PASSWORD}@localhost/fastapi"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+    # , connect_args={"check_same_thread": False} - used only for SQLite
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        # pass
+        db.close(
