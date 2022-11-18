8c5cf5dcb722262ef73e9a67e028d76c11aa47ab
oluwasube <oluwasube@outlook.com>
2022-11-18 18:14:02 +0100

    feat(BAC-21):created the API core endpoint

--- backend/app/routers/weather.py
+++ backend/app/routers/weather.py
@@ -1,17 +1,43 @@
 # implement main application logic like weather api response here
 
 # FastApi imports
-from typing import List
-from fastapi import Response, status, HTTPException, Depends, APIRouter
-
+from fastapi import status, HTTPException, APIRouter
+import requests
 # Internal import
-from models import User
-from schemas import *
-from database import db_engine, get_db
-from utils import generate_hash
+from .. import utils 
 
-# Sqlalchemy import
 
 router = APIRouter(
     tags=['weather']
 )
+
+
+@router.post("/weather")
+def get_current_weather():
+    
+    city_name = requests.json['city_name']
+    lga = requests.json['lga']
+    state = requests.json['state']
+    
+    long_lat = utils.geocode_address(city_name, lga, state)
+    
+    if long_lat is None:
+        
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail= f"invalid longitute and latitude"
+        
+        )
+
+    
+    lat, lon = long_lat
+    return utils.weather_api_call(lon, lat)
+    
+
+
+    
+
+
+
+
+
