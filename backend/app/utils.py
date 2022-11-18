0da456943668d52f2fc721d243f75fee80294b52
chiefkufre <samuelkufrewillie@gmail.com>
2022-11-18 15:42:03 +0100

    feat(BE-20): add fnction to accept latitude and longitude and return weather conditions

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,5 +1,8 @@
 
 from decouple import config
+from fastapi import HTTPException, status
+
+import requests
 
 # Utility function to generate and verify password hash
 from passlib.context import CryptContext
@@ -32,12 +35,47 @@ def verify_password_hash(password: str, hash_password: str):
 
 
 
-# feature to call the open weather api and fetch the required data
-
-
-
-def waether_api_call(lon, lat, *args, **kwargs):
+# function to call the open weather api and fetch the required data
+# Required data = "lon" and "lat"
+def weather_api_call(lon, lat, *args, **kwargs):
 
     API_key = config("API_KEY")
-    part = "alert"
-    open_weather_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API_key}"
+
+    # converts given parameters into required types
+    lon = float(lon)
+    lat = float(lat)
+
+    # Call API and converts response into dictionary
+    open_weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}"
+
+    try:
+        response = requests.get(open_weather_url).json()
+
+        # Error messages for unknown city or staet names or invalid API key
+        if response.status_code != 200:
+            return f"Can't retrive weather data for this location"
+        
+        weather_conditions = response['weather'] #returns a lists
+
+        for detail in weather_conditions:
+            current_weather = detail['main']
+            weather_description = detail['description']
+        
+        return {
+            "current_weather": current_weather,
+            "weather_description": weather_description
+        }
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail= f"Weather conditon not found.Please retry again"
+        
+        )
+
+
+
+
+
+
+
+
