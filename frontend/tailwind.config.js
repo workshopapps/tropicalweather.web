1207afb549390d5a38282a2d9ccfc6061e7cf637
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2022-11-19 23:00:22 +0100

    deploying about page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      screens: {
+        sm: '320px',
+        // => @media (min-width: 320px) { ... }
+
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
       colors: {
         'primary-btn': '#EF6820',
         'primary-btn-clicked': '#F7B27A',
