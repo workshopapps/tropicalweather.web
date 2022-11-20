41d7a66091f1fd252b1f356d7e6742231063d266
AkintolaRichard <princerichard17a@gmail.com>
2022-11-20 22:13:19 +0100

    fix: refactor immediate forecasts endpoint
    Change how the endpoint parse the json from the openweathermap api

--- backend/app/tests/test_weather_forecasts_immediate.py
+++ backend/app/tests/test_weather_forecasts_immediate.py
@@ -17,173 +17,33 @@ class TestGetImmediateWeatherForecast:
                 status_code=200,
                 json=mocker.Mock(
                     return_value={
-                        "cod": "200",
-                        "message": 0,
-                        "cnt": 40,
-                        "list": [{
-                            "dt": 1661871600,
-                            "main": {
-                                "temp": 296.76,
-                                "feels_like": 296.98,
-                                "temp_min": 296.76,
-                                "temp_max": 297.87,
-                                "pressure": 1015,
-                                "sea_level": 1015,
-                                "grnd_level": 933,
-                                "humidity": 69,
-                                "temp_kf": -1.11},
-                            "weather": [{
-                                "id": 500,
-                                "main": "Rain",
-                                "description": "light rain",
-                                "icon": "10d"}],
-                            "clouds": {
-                                "all": 100
-                                },
-                            "wind": {
-                                "speed": 0.62,
-                                "deg": 349,
-                                "gust": 1.18
-                                },
-                        "visibility": 10000,
-                        "pop": 0.32,
-                        "rain": {
-                            "3h": 0.26
-                        },
-                        "sys": {
-                            "pod": "d"
-                        },
-                        "dt_txt": "2022-08-30 15:00:00"
-                        },
-                        {
-                        "dt": 1661882400,
-                        "main": {
-                            "temp": 295.45,
-                            "feels_like": 295.59,
-                            "temp_min": 292.84,
-                            "temp_max": 295.45,
-                            "pressure": 1015,
-                            "sea_level": 1015,
-                            "grnd_level": 931,
-                            "humidity": 71,
-                            "temp_kf": 2.61
-                        },
-                        "weather": [
-                            {
-                            "id": 500,
-                            "main": "Rain",
-                            "description": "light rain",
-                            "icon": "10n"
-                            }
-                        ],
-                        "clouds": {
-                            "all": 96
-                        },
-                        "wind": {
-                            "speed": 1.97,
-                            "deg": 157,
-                            "gust": 3.39
-                        },
-                        "visibility": 10000,
-                        "pop": 0.33,
-                        "rain": {
-                            "3h": 0.57
-                        },
-                        "sys": {
-                            "pod": "n"
-                        },
-                        "dt_txt": "2022-08-30 18:00:00"
-                        },
-                        {
-                        "dt": 1661893200,
-                        "main": {
-                            "temp": 292.46,
-                            "feels_like": 292.54,
-                            "temp_min": 290.31,
-                            "temp_max": 292.46,
-                            "pressure": 1015,
-                            "sea_level": 1015,
-                            "grnd_level": 931,
-                            "humidity": 80,
-                            "temp_kf": 2.15
-                        },
-                        "weather": [
-                            {
-                            "id": 500,
-                            "main": "Rain",
-                            "description": "light rain",
-                            "icon": "10n"
-                            }
-                        ],
-                        "clouds": {
-                            "all": 68
-                        },
-                        "wind": {
-                            "speed": 2.66,
-                            "deg": 210,
-                            "gust": 3.58
-                        },
-                        "visibility": 10000,
-                        "pop": 0.7,
-                        "rain": {
-                            "3h": 0.49
-                        },
-                        "sys": {
-                            "pod": "n"
-                        },
-                        "dt_txt": "2022-08-30 21:00:00"
-                        },
-                        {
-                        "dt": 1662292800,
-                        "main": {
-                            "temp": 294.93,
-                            "feels_like": 294.83,
-                            "temp_min": 294.93,
-                            "temp_max": 294.93,
-                            "pressure": 1018,
-                            "sea_level": 1018,
-                            "grnd_level": 935,
-                            "humidity": 64,
-                            "temp_kf": 0
-                        },
-                        "weather": [
-                            {
-                            "id": 804,
-                            "main": "Clouds",
-                            "description": "overcast clouds",
-                            "icon": "04d"
-                            }
-                        ],
-                        "clouds": {
-                            "all": 88
-                        },
-                        "wind": {
-                            "speed": 1.14,
-                            "deg": 17,
-                            "gust": 1.57
-                        },
-                        "visibility": 10000,
-                        "pop": 0,
-                        "sys": {
-                            "pod": "d"
-                        },
-                        "dt_txt": "2022-09-04 12:00:00"
-                        }
-                    ],
-                    "city": {
-                        "id": 3163858,
-                        "name": "Zocca",
-                        "coord": {
-                        "lat": 44.34,
-                        "lon": 10.99
-                        },
-                        "country": "IT",
-                        "population": 4593,
-                        "timezone": 7200,
-                        "sunrise": 1661834187,
-                        "sunset": 1661882248
-                    }
-                    }
+                        'base': 'stations',
+                        'clouds': {'all': 33},
+                        'cod': 200,
+                        'coord': {'lat': 6.5227, 'lon': 3.6218},
+                        'dt': 1668966061,
+                        'id': 2332453,
+                        'main': {'feels_like': 308.4,
+                                'grnd_level': 1009,
+                                'humidity': 80,
+                                'pressure': 1009,
+                                'sea_level': 1009,
+                                'temp': 302.33,
+                                'temp_max': 302.33,
+                                'temp_min': 302.33},
+                        'name': 'Lagos',
+                        'sys': {'country': 'NG',
+                                'id': 1185,
+                                'sunrise': 1668922625,
+                                'sunset': 1668965126,
+                                'type': 1},
+                        'timezone': 3600,
+                        'visibility': 10000,
+                        'weather': [{'description': 'scattered clouds',
+                                    'icon': '03n',
+                                    'id': 802,
+                                    'main': 'Clouds'}],
+                        'wind': {'deg': 209, 'gust': 7.96, 'speed': 5.59}}
                 )
             )
         )
@@ -191,7 +51,7 @@ class TestGetImmediateWeatherForecast:
         response = client.get("/weather/forecasts/immediate?lat=3.4&lng=45.9")
         data: dict = response.json()
         assert len(data) == 4
-        assert data["date"] == "30 Aug, 2022"
-        assert data["time"] == "04:00pm"
-        assert data["main"] == "Rain"
-        assert data["description"] == "light rain"
+        assert data["date"] == "20 Nov, 2022"
+        assert data["time"] == "06:41pm"
+        assert data["main"] == "Clouds"
+        assert data["description"] == "scattered clouds"
