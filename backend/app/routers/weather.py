f9c0ef0521bbff07aa63e6fa88794f207c8874c3
oluwasube <oluwasube@outlook.com>
2022-11-20 16:13:30 +0100

    feat/BAC-30-Implement core endpoints

--- backend/app/routers/weather.py
+++ backend/app/routers/weather.py
@@ -8,6 +8,8 @@ from fastapi import APIRouter
 from fastapi import HTTPException, status
 # Internal import
 from app.schemas import *  # noqa: F401, F403
+import requests
+from app.utils import  weather_api_call, geocode_address
 
 from app.utils import (
     get_weather_forecast,
@@ -54,3 +56,52 @@ async def immediate_weather_forecast(lat: float=None, lng: float=None):
     result = get_immediate_weather_api_call(lat, lng)
 
     return result
+
+
+@router.post("/weather/current")
+def get_current_weather():
+    
+    city_name = requests.json['city_name']
+    state = requests.json['state']
+    
+    long_lat = geocode_address(city_name, state)
+    
+    if long_lat is None:
+        
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail= f"Can't retrive weather data for this location"
+        
+        )
+    
+    lat = long_lat['lat']
+    lng= long_lat['lng']
+   # city = long_lat['city']
+   # state = long_lat['state']
+    
+    result = weather_api_call(lat, lng)
+     
+    if result is None:
+        
+       raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Weather conditon not found.Please retry again"
+        )
+
+    
+    weather_condition = result['main']
+    description =result['description']
+    date = result['dt']
+    state = result['state']
+    city = result['city']
+    time = result['time']
+    
+    return {
+        
+    "state": state,
+    "city": city,
+    "main": weather_condition,
+    "description": description,
+    "date": date,
+    "time": time    
+    }
