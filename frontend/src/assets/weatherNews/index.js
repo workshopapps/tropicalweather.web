346a58dbe19398d1d902015923bb939d1e8be755
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2022-11-20 08:21:01 +0100

    fix: removes unused assets

--- frontend/src/assets/weatherNews/index.js
+++ frontend/src/assets/weatherNews/index.js
@@ -1,15 +1,3 @@
-import logoFooter from './footerLogo.svg';
-import appstore from './appstore.svg';
-import playstore from './playstore.svg';
-import logo from './logo.svg';
-import home from './home.svg';
-import features from './features.svg';
-import briefcase from './briefcase.svg';
-import info from './info.svg';
-import signup from './signup.svg';
-import search from './search.svg';
-import closeNav from './closeNav.svg';
-import openNav from './openNav.svg';
 import news1 from './news1.svg';
 import news3 from './news3.svg';
 import news2 from './news2.svg';
@@ -20,18 +8,6 @@ import news1Vert from './news1-vert.svg';
 import news2Vert from './news2-vert.svg';
 
 export {
-  logoFooter,
-  appstore,
-  playstore,
-  logo,
-  search,
-  closeNav,
-  openNav,
-  home,
-  features,
-  info,
-  briefcase,
-  signup,
   news1,
   news3,
   news2,
