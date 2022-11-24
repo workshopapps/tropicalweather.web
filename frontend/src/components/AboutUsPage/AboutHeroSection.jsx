af6e1e3d4eac755326e1664babb59b86aee73e05
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-11-24 10:03:43 +0100

    fix: code cleanup

--- frontend/src/components/AboutUsPage/AboutHeroSection.jsx
+++ frontend/src/components/AboutUsPage/AboutHeroSection.jsx
@@ -4,16 +4,15 @@ function AboutHeroSection() {
   return (
     <div className="">
       <div className="space-y-6 font-Outfit">
-        <p className="font-bold text-2xl md:text-6xl text-center text-[#2B2A30] md:pt-20 py-10">
+        <h1 className="font-bold text-2xl md:text-6xl text-center text-[#2B2A30] md:pt-20 py-10">
           We combine cutting-edge weather technology with human insight to accurately
           forecast weather and improve lives
-        </p>
+        </h1>
       </div>
       <div className="">
         <img
           src="AboutAsset/Frame 33849.png"
           alt=""
-          className=""
         />
       </div>
     </div>
