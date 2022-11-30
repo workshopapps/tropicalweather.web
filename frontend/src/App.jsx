999dbb27e13492ea1799dca4979dc3c3c761a5f2
Adewoyin Aanuoluwapo <adewoyinaanuoluwapo@gmail.com>
2022-12-01 00:21:29 +0100

    fix: notification settings page styled

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,8 @@ import Notification from './pages/Notification';
 import AirQuality from './pages/AirQuality';
 import FullWeatherDetails from './pages/FullWeatherDetails';
 import LandingPage from './pages/LandingPage';
+import Notificationsettings from './pages/NotificationSettings';
+
 // import Promotions from './pages/Promotions';
 
 const queryClient = new QueryClient();
@@ -27,6 +29,8 @@ const router = createBrowserRouter(
       <Route path="/air-quality" element={<AirQuality />} />
       <Route path="/weather-details" element={<FullWeatherDetails />} />
       <Route path="/landing" element={<LandingPage />} />
+      <Route path="/notificationsettings" element={<Notificationsettings />} />
+
       {/* <Route path="/promotions" element={<Promotions />} /> */}
     </Route>
   )
--- frontend/src/pages/Notificationsettings.jsx
+++ frontend/src/pages/Notificationsettings.jsx
@@ -1,7 +1,63 @@
 import React from 'react';
+import '../styles/NotificationSettings.css';
+import BackIcon from '../NotificationSettingsAssets/BackIcon.svg';
 
 export default function Notificationsettings() {
   return (
-    <div>testgit nfdoiomomdpofpomgit</div>
+    <div className="notificationsettings__container">
+      <div className="return__btn--container">
+        <img src={BackIcon} alt="Back Icon" />
+        <p>Back</p>
+      </div>
+      <div className="notification__header">
+        <h2>Notifications</h2>
+        <p>Choose when and how we can contact you</p>
+      </div>
+      <div className="notificationsettings">
+        <div className="notificationsetting">
+          <div className="notificationsetting__header">
+            <h3>Reminders</h3>
+            <p>These are notification to remind you of updates you might have missed</p>
+          </div>
+          <div className="notificationsetting__option">
+            <input type="radio" className="radio" />
+            <h5 className="notificationsetting__option--heading-text">Do not notify me</h5>
+          </div>
+          <div className="notificationsetting__option1">
+            <input type="radio" className="radio" />
+            <div>
+              <h5 className="notificationsetting__option--heading-text">Important reminders only</h5>
+              <p className="notificationsetting__option--description">Only notify me if the reminder is tagged as important</p>
+            </div>
+          </div>
+          <div className="notificationsetting__option2">
+            <input type="radio" className="radio" />
+            <div>
+              <h5 className="notificationsetting__option--heading-text">All reminders</h5>
+              <p className="notificationsetting__option--description">Notify me for all reminders</p>
+            </div>
+          </div>
+        </div>
+
+        <section className="notificationsetting">
+          <div className="notificationsetting__header">
+            <h3>More activity about you</h3>
+            <p>These are notifications for posts on your profile,
+              details and list of notifications</p>
+          </div>
+          <div className="notificationsetting__option">
+            <input type="radio" className="radio" />
+            <h5 className="notificationsetting__option--heading-text">Do not notify me</h5>
+          </div>
+          <div className="notificationsetting__option2">
+            <input type="radio" className="radio" />
+            <div>
+              <h5 className="notificationsetting__option--heading-text">All reminders</h5>
+              <p className="notificationsetting__option--description">Notify me for all reminders</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
   );
 }
