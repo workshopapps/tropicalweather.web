c954576f2d07828e7f62a937448f45debbda5118
ibimina <96787210+ibimina@users.noreply.github.com>
2022-11-30 20:20:10 +0100

    docs: notification feed page

--- frontend/src/pages/NotificationFeedList.jsx
+++ frontend/src/pages/NotificationFeedList.jsx
@@ -1,3 +1,24 @@
+import { Link } from 'react-router-dom';
+import NoNotificationFeed from '../components/NotificationFeedlist/NoNotificationFeed';
+import '../styles/NotificationFeedList.css';
+
 export default function NotificationFeedList() {
-    return (<> </>);
+    return (
+      <div className="notificaton_feed-container">
+        <div className="notificaton_feedhd">
+          <Link
+            to="/"
+            className="notificaton_feedbg notificaton_feedhome"
+            aria-label="home"
+          />
+          <h1>Notification</h1>
+          <button
+            type="button"
+            className="notificaton_feedbg notificaton_feedtrash"
+            aria-label="trash icon"
+          />
+        </div>
+        <NoNotificationFeed />
+      </div>
+    );
 }
