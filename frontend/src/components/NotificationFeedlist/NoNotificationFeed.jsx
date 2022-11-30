78d55c9489e7841acfc7f78b9c5b8ef4125b0fca
ibimina <96787210+ibimina@users.noreply.github.com>
2022-11-30 20:18:07 +0100

    docs: no notification feed or empty feed component

--- frontend/src/components/NotificationFeedlist/NoNotificationFeed.jsx
+++ frontend/src/components/NotificationFeedlist/NoNotificationFeed.jsx
@@ -1,3 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
 export default function NoNotificationFeed() {
- return (<> </>);
+ return (
+   <div className="notificaton_feed-nolist">
+     <div>
+       <img
+         src="./assets/NotificationFeedList/trash-empty.svg"
+         alt="a trash icon"
+         className="notificaton_feed-bigtrash"
+       />
+       <p className="notificaton_feed-empty">Empty</p>
+       <p>
+         Nothing to see here When you receive weather updates, they'll appear here.
+       </p>
+     </div>
+   </div>
+ );
 }
