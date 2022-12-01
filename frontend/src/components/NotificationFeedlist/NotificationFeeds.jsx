ba5cb6916444599fbe29b7e29039fab426a56866
ibimina <96787210+ibimina@users.noreply.github.com>
2022-12-01 12:54:32 +0100

    docs: conditional dispaly of weather events

--- frontend/src/components/NotificationFeedlist/NotificationFeeds.jsx
+++ frontend/src/components/NotificationFeedlist/NotificationFeeds.jsx
@@ -5,10 +5,39 @@ export default function NotificationFeeds({ feeds }) {
   return (
     <>
       <ul>
-
         {feeds.map((news) => (
-          <li key={news.id}>
-            <p>{news.events}</p>
+          <li key={news.id} className="notificaton_feed-list">
+            {news.events === 'rainy' && (
+              <>
+                <div className="notificaton_feed-listcontent">
+                  <img src="./assets/NotificationFeedList/icon.svg" alt="" />
+                  <p className="notificaton_feed-feed">{news.feed}</p>
+                </div>
+                <p className="notificaton_feed-time">{news.time}</p>
+              </>
+            )}
+
+            {news.events === 'sunny' && (
+              <>
+                <div className="notificaton_feed-listcontent">
+                  <img
+                    src="./assets/NotificationFeedList/Ellipse 14.svg"
+                    alt=""
+                  />
+                  <p className="notificaton_feed-feed">{news.feed}</p>
+                </div>
+                <p className="notificaton_feed-time">{news.time}</p>
+              </>
+            )}
+            {news.events === 'cloudy' && (
+              <>
+                <div className="notificaton_feed-listcontent">
+                  <img src="./assets/NotificationFeedList/cloudy.svg" alt="" />
+                  <p className="notificaton_feed-feed">{news.feed}</p>
+                </div>
+                <p className="notificaton_feed-time">{news.time}</p>
+              </>
+            )}
           </li>
         ))}
       </ul>
