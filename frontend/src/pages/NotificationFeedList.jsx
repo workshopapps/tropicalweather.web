1afc13e80b4edd717e50c01d2078f5ceb50da02a
ibimina <96787210+ibimina@users.noreply.github.com>
2022-12-01 10:36:25 +0100

    docs: added a boolean state to show/ hide modal

--- frontend/src/pages/NotificationFeedList.jsx
+++ frontend/src/pages/NotificationFeedList.jsx
@@ -1,72 +1,77 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import NotificationFeeds from '../components/NotificationFeedlist/NotificationFeeds';
 import NoNotificationFeed from '../components/NotificationFeedlist/NoNotificationFeed';
 import '../styles/NotificationFeedList.css';
+import DeleteNotification from '../components/NotificationFeedlist/DeleteNotification';
 
 export default function NotificationFeedList() {
-  // const {latitude,longtitude}="";
-const sampleWeatherFeed = [
-  { id: 'a',
-    events: 'rainy ',
-    feed: 'There will be heavy rain for some in northeast with a few brighter exceptions',
-    time: 'Now',
-  },
-  {
-    id: 'b',
-    events: 'rainy ',
-    feed: 'There will be heavy rain for some in northeast with a few brighter exceptions',
-    time: 'Now',
-  },
+  const [show, setShow] = useState(false);
+  const sampleWeatherFeed = [
+    {
+      id: 'a',
+      events: 'rainy ',
+      feed: 'There will be heavy rain for some in northeast with a few brighter exceptions',
+      time: 'Now',
+    },
+    {
+      id: 'b',
+      events: 'rainy ',
+      feed: 'There will be heavy rain for some in northeast with a few brighter exceptions',
+      time: 'Now',
+    },
 
-  {
-    id: 'c',
-    events: ' sunny',
-    feed: 'There wil be heavy rain for some in northeast with a few brighter exceptions',
-    time: '12 mins',
-  },
-  {
-    id: 'd',
-    events: 'rainy ',
-    feed: ' Showers or longer spells of rain from Saturday night onwards, with some strong winds.',
-    time: '3 hours',
-  },
-  {
-    id: 'e',
-    events: 'cloudy ',
-    feed: 'There will be dark clouds and skies today. The weather remains cool all through the day.',
-    time: '8 hours',
-  },
-  {
-    id: 'f',
-    events: 'sunny',
-    feed: 'The sun today will be scorching. You stand the risk of being exposed to high dust levels.',
-    time: '12 hours',
-  },
-];
-    return (
-      <div className="notificaton_feed-container">
-        <div className="notificaton_feedhd">
-          <div className="notificaton_feedhd-flex">
-            <Link
-              to="/"
-              className="notificaton_feedbg notificaton_feedhome"
-              aria-label="home"
-            >
-              <span>Back</span>
-            </Link>
-            <h1>Notification</h1>
-          </div>
-
-          <button
-            type="button"
-            className="notificaton_feedbg notificaton_feedtrash"
-            aria-label="trash icon"
-          />
+    {
+      id: 'c',
+      events: ' sunny',
+      feed: 'There wil be heavy rain for some in northeast with a few brighter exceptions',
+      time: '12 mins',
+    },
+    {
+      id: 'd',
+      events: 'rainy ',
+      feed: ' Showers or longer spells of rain from Saturday night onwards, with some strong winds.',
+      time: '3 hours',
+    },
+    {
+      id: 'e',
+      events: 'cloudy ',
+      feed: 'There will be dark clouds and skies today. The weather remains cool all through the day.',
+      time: '8 hours',
+    },
+    {
+      id: 'f',
+      events: 'sunny',
+      feed: 'The sun today will be scorching. You stand the risk of being exposed to high dust levels.',
+      time: '12 hours',
+    },
+  ];
+  return (
+    <div className="notificaton_feed-container">
+      <div className="notificaton_feedhd">
+        <div className="notificaton_feedhd-flex">
+          <Link
+            to="/"
+            className="notificaton_feedbg notificaton_feedhome"
+            aria-label="home"
+          >
+            <span>Back</span>
+          </Link>
+          <h1>Notification</h1>
         </div>
-        {sampleWeatherFeed.length > 1 && (
-          <NotificationFeeds feeds={sampleWeatherFeed} />
-        )}
-        {sampleWeatherFeed.length < 1 && <NoNotificationFeed />}
+
+        <button
+          type="button"
+          className="notificaton_feedbg notificaton_feedtrash"
+          aria-label="trash icon"
+          onClick={() => (!show ? setShow(true) : setShow(false))}
+        />
       </div>
-    );
+      {sampleWeatherFeed.length > 1 && (
+        <NotificationFeeds feeds={sampleWeatherFeed} />
+      )}
+      {sampleWeatherFeed.length < 1 && <NoNotificationFeed />}
+      <DeleteNotification show={show} />
+    </div>
+  );
 }
