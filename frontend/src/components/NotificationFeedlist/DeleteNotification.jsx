f00915b69522fb9506db945eae3c1606c95fe99d
ibimina <96787210+ibimina@users.noreply.github.com>
2022-12-01 12:52:22 +0100

    docs: delete modal data

--- frontend/src/components/NotificationFeedlist/DeleteNotification.jsx
+++ frontend/src/components/NotificationFeedlist/DeleteNotification.jsx
@@ -1,25 +1,33 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
-/* eslint-disable react/button-has-type */
-export default function DeleteNotification({ show }) {
-    return (
-      <div
-        className={
-          show ? 'notificaton_feed-delecon' : 'notificaton_feed-none'
-        }
-      >
-        <div className="notificaton_feed-dele">
-          <h1>Are you sure?</h1>
-          <p>
-            Are you sure you want to empty your notifications. You'll lose
-            access to them
-          </p>
-          <button className="notificaton_feed-cancel">Cancel</button>
-          <button className="notificaton_feed-empty">Empty now</button>
+export default function DeleteNotification({ show, closeModal, emptyNotification }) {
+  return (
+    <div
+      className={show ? 'notificaton_feed-delecon' : 'notificaton_feed-none'}
+    >
+      <div className="notificaton_feed-dele">
+        <h1 className="notificatin_feed-hd">Are you sure?</h1>
+        <p>
+          Are you sure you want to empty your notifications. You'll lose access
+          to them
+        </p>
+        <div className="notificaton_feed-btnflex">
+          <button
+            className="notificaton_feed-btn notificaton_feed-cancel"
+            type="button"
+            onClick={closeModal}
+          >
+            Cancel
+          </button>
+          <button
+            className="notificaton_feed-btn notificaton_feed-empty"
+            type="button"
+            onClick={emptyNotification}
+          >
+            Empty now
+          </button>
         </div>
       </div>
-    );
+    </div>
+  );
 }
-// DeleteNotification.prototype = {
-//     show: prototype.boolean.isRequired
-// };
