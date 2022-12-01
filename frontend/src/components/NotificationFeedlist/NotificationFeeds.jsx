2f818f0742a157b6faaa768601a1b707fb08046e
ibimina <96787210+ibimina@users.noreply.github.com>
2022-12-01 10:16:33 +0100

    docs: notification feeds data list component

--- frontend/src/components/NotificationFeedlist/NotificationFeeds.jsx
+++ frontend/src/components/NotificationFeedlist/NotificationFeeds.jsx
@@ -1,3 +1,17 @@
-export default function NotificationFeed() {
- return (<> </>);
+/* eslint-disable react/prop-types */
+
+// eslint-disable-next-line react/prop-types
+export default function NotificationFeeds({ feeds }) {
+  return (
+    <>
+      <ul>
+
+        {feeds.map((news) => (
+          <li key={news.id}>
+            <p>{news.events}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
