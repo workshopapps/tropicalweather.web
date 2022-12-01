45462aa88ed543e7655f48f3490df85cf015717b
ibimina <96787210+ibimina@users.noreply.github.com>
2022-12-01 12:55:42 +0100

    docs: add boolean to show delete modal

--- frontend/src/pages/NotificationFeedList.jsx
+++ frontend/src/pages/NotificationFeedList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import NotificationFeeds from '../components/NotificationFeedlist/NotificationFeeds';
@@ -6,36 +7,36 @@ import '../styles/NotificationFeedList.css';
 import DeleteNotification from '../components/NotificationFeedlist/DeleteNotification';
 
 export default function NotificationFeedList() {
-  const [show, setShow] = useState(false);
-  const sampleWeatherFeed = [
+  const locationn = [];
+  const [location, setLocation] = useState([
     {
       id: 'a',
-      events: 'rainy ',
+      events: 'rainy',
       feed: 'There will be heavy rain for some in northeast with a few brighter exceptions',
       time: 'Now',
     },
     {
       id: 'b',
-      events: 'rainy ',
+      events: 'rainy',
       feed: 'There will be heavy rain for some in northeast with a few brighter exceptions',
-      time: 'Now',
+      time: '5 mins',
     },
 
     {
       id: 'c',
-      events: ' sunny',
+      events: 'sunny',
       feed: 'There wil be heavy rain for some in northeast with a few brighter exceptions',
       time: '12 mins',
     },
     {
       id: 'd',
-      events: 'rainy ',
+      events: 'rainy',
       feed: ' Showers or longer spells of rain from Saturday night onwards, with some strong winds.',
       time: '3 hours',
     },
     {
       id: 'e',
-      events: 'cloudy ',
+      events: 'cloudy',
       feed: 'There will be dark clouds and skies today. The weather remains cool all through the day.',
       time: '8 hours',
     },
@@ -45,33 +46,40 @@ export default function NotificationFeedList() {
       feed: 'The sun today will be scorching. You stand the risk of being exposed to high dust levels.',
       time: '12 hours',
     },
-  ];
-  return (
-    <div className="notificaton_feed-container">
-      <div className="notificaton_feedhd">
-        <div className="notificaton_feedhd-flex">
-          <Link
-            to="/"
-            className="notificaton_feedbg notificaton_feedhome"
-            aria-label="home"
-          >
-            <span>Back</span>
-          </Link>
-          <h1>Notification</h1>
-        </div>
+  ]);
 
-        <button
-          type="button"
-          className="notificaton_feedbg notificaton_feedtrash"
-          aria-label="trash icon"
-          onClick={() => (!show ? setShow(true) : setShow(false))}
-        />
+  const [show, setShow] = useState(false);
+  const closeModal = () => {
+    setShow(false);
+  };
+   const emptyNotification = () => {
+    setLocation([]);
+        setShow(false);
+   };
+    return (
+      <div className="notificaton_feed-container">
+        <div className="notificaton_feedhd">
+          <div className="notificaton_feedhd-flex">
+            <Link
+              to="/"
+              className="notificaton_feedbg notificaton_feedhome"
+              aria-label="home"
+            >
+              <span>Back</span>
+            </Link>
+            <h1>Notification</h1>
+          </div>
+
+          <button
+            type="button"
+            className="notificaton_feedbg notificaton_feedtrash"
+            aria-label="trash icon"
+            onClick={() => (!show && location.length > 0 ? setShow(true) : setShow(false))}
+          />
+        </div>
+        {location.length > 1 && <NotificationFeeds feeds={location} />}
+        {location.length < 1 && <NoNotificationFeed />}
+        <DeleteNotification show={show} location={location} closeModal={closeModal} emptyNotification={emptyNotification} />
       </div>
-      {sampleWeatherFeed.length > 1 && (
-        <NotificationFeeds feeds={sampleWeatherFeed} />
-      )}
-      {sampleWeatherFeed.length < 1 && <NoNotificationFeed />}
-      <DeleteNotification show={show} />
-    </div>
-  );
+    );
 }
