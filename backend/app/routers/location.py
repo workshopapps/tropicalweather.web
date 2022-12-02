671c9744a66abca2bd4cd876d68e61607c325a88
bolexs <65228354+bolexs@users.noreply.github.com>
2022-12-02 22:03:42 +0100

    Revert "Feat/bac 97 create endpoint for /weather/forecast/extended (#169)"
    This reverts commit 58a2a54e33bec834c74eee042be792cc61c94406.

--- backend/app/routers/location.py
+++ backend/app/routers/location.py
@@ -9,22 +9,21 @@ router = APIRouter(
 
 @router.get('/location', response_model=locationResponse)
 async def get_location(lat: float, lon: float):
-    
-       
-        address : list(dict(str,str)) = reverse_geocoding(lat, lon)
-        if address is None: 
-            
-            raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, 
-            detail= "Invalid-Parameters")
-        
-        for location in address:
-            city = location.get('name')
-            state = location.get('state')
+    try:
+        reverse_geocode = reverse_geocoding(lat, lon)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Location does not exist"
+        )
 
-        response = {
+    for location in reverse_geocode:
+        city = location.get('name')
+        state = location.get('state')
+
+    response = {
         'city': city,
         'state': state
-        }
+    }
 
-        return response
+    return response
