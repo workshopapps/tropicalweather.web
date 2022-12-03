d286c1874d058ccda16c0e8a397409a875bf8c8d
chiefkufre <samuelkufrewillie@gmail.com>
2022-12-03 09:33:33 +0100

    chore: refactor datetiime format for push notification

--- backend/app/utils/push_notification.py
+++ backend/app/utils/push_notification.py
@@ -17,7 +17,7 @@ def send_notification_to_topic(payload, topic):
         data={
         "event": payload['event'],
         "message": payload["description"],
-        "datetime": datetime.strptime(payload['end'], '%y-%m-%d %H:%M')
+        "datetime": datetime.strptime(payload['end'], '%Y-%m-%d %H:%M')
         },
         topic=topic,
     )
