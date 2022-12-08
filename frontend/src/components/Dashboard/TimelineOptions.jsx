c7337ea71a8cf62b1a48cb7fc313ff9a667b6f59
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-12-08 01:43:53 +0100

    feat: add options popup for weather timeline

---
+++ frontend/src/components/Dashboard/TimelineOptions.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BsCalendar4Week, BsCalendar4Event } from 'react-icons/bs';
+import { MdOutlineViewWeek } from 'react-icons/md';
+
+export default function TimelineOptions({ display, setTimeline }) {
+  return (
+    <div
+      className={`${display ? 'grid grid-cols-1' : 'hidden'
+        } p-3 shadow dropdown-content menu bg-base-100 rounded-box w-52 absolute top-10 right-0 divide-y bg-white`}
+    >
+      <button
+        type="button"
+        title="today"
+        className="flex items-center gap-2 py-2 cursor-pointer hover:text-primary-btn"
+        onClick={() => {
+          setTimeline('today');
+        }}
+      >
+        <MdOutlineViewWeek className="text-xl" />
+        <p>Today</p>
+      </button>
+      <button
+        type="button"
+        title="tomorrow"
+        className="flex items-center gap-2 py-2 cursor-pointer hover:text-primary-btn"
+        onClick={() => {
+          setTimeline('tomorrow');
+        }}
+      >
+        <BsCalendar4Event className="text-xl" />
+        <p>Tomorrow</p>
+      </button>
+      <button
+        type="button"
+        title="weekly"
+        className="flex items-center gap-2 py-2 cursor-pointer hover:text-primary-btn"
+        onClick={() => {
+          setTimeline('weekly');
+        }}
+      >
+        <BsCalendar4Week className="text-xl" />
+        <p>Weekly</p>
+      </button>
+    </div>
+  );
+}
+
+TimelineOptions.propTypes = {
+  display: PropTypes.bool.isRequired,
+  setTimeline: PropTypes.func.isRequired,
+};
