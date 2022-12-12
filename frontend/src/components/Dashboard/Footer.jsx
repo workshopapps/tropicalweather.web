7b4810055d2673c3d4e35d03643218dd45119109
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-12-12 12:43:11 +0100

    fix: mobile font sizing

--- frontend/src/components/Dashboard/Footer.jsx
+++ frontend/src/components/Dashboard/Footer.jsx
@@ -1,26 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import i18next from 'i18next';
 import { useTranslation } from 'react-i18next';
 import { BsGlobe } from 'react-icons/bs';
 import ReactThemeToggleButton from '../Settings/ToggleTheme';
 
 export default function Footer() {
-  const { t } = useTranslation(['common']);
+  function getLanguageValues() {
+    const storedLanguageValue = localStorage.getItem('i18nextLng');
+    if (!storedLanguageValue) return '';
+    return storedLanguageValue;
+  }
+  const { i18n, t } = useTranslation(['common']);
+  const [language, setLanguage] = useState(getLanguageValues);
 
+  useEffect(() => {
+    localStorage.setItem('i18nextLng', language);
+  }, [language]);
+
+  const languageData = [
+    {
+      title: `${t('narabic')}`,
+      langVal: 'ar',
+    },
+    {
+      title: `${t('nenglish')}`,
+      langVal: 'en',
+    },
+    {
+      title: `${t('nfrench')}`,
+      langVal: 'fr',
+    },
+    {
+      title: `${t('nspanish')}`,
+      langVal: 'es',
+    },
+  ];
+
+  useEffect(() => {
+    if (localStorage.getItem('i18nextLang')?.length > 2) {
+      i18next.changeLanguage('en');
+    }
+  });
+
+  const handleLanguageChange = (e) => {
+    i18n.changeLanguage(e.target.value);
+    setLanguage(e.currentTarget.value);
+  };
   return (
-    <div className="fixed bottom-0 left-0 right-0 flex flex-col justify-between w-full gap-4 px-4 py-5 bg-white border-t border-t-white md:flex-row md:px-16 bg-[var(--d-bg)]">
-      <p className="text-sm md:order-first md:text-lg">
+    <div className="md:text-sm text-xs fixed bottom-0 left-0 right-0 flex flex-col justify-between w-full md:gap-4 px-4 py-2 md:py-5 bg-white border-t border-t-white md:flex-row md:px-16 bg-[var(--d-bg)] gap-2">
+      <p className="md:order-first md:text-lg">
         {t('allrights')}
       </p>
-      <div className="flex gap-6 items-center">
+      <div className="flex items-center gap-6">
         <span className="flex items-center gap-2">
           <BsGlobe />
-          <span className="hidden md:block">English</span>
+          <select value={language} onChange={handleLanguageChange} className="outline-none focus:outline-none">
+            {languageData?.map(({ title, langVal }) => (
+              <option
+                value={langVal}
+                key={langVal}
+              >
+                {title}
+              </option>
+            ))}
+          </select>
         </span>
         <span className="flex items-center gap-2">
-          <img src="/icons/naija-flag.png" alt="Nigeria flag" />
-          <span className="hidden md:block">Nigeria</span>
+          <img src={`/icons/${language}-flag.png`} alt={`${language} flag`} />
         </span>
-        <span className="h-max pt-1">
+        <span className="pt-1 h-max">
           <ReactThemeToggleButton />
         </span>
       </div>
